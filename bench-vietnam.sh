0b6f8d08555f429fd6b8c2dd80aef6100aa882d0
wpbai <139647725+wpbai@users.noreply.github.com>
2023-08-20 00:37:57 +0700

    Update bench-vietnam.sh

--- bench-vietnam.sh
+++ bench-vietnam.sh
@@ -54,49 +54,21 @@ get_opsy() {
 next() {
     printf "%-70s\n" "-" | sed 's/\s/-/g'
 }
-
-speed_test() {
-    local nodeName="$2"
-    [ -z "$1" ] && ./speedtest-cli/speedtest --progress=no --accept-license --accept-gdpr > ./speedtest-cli/speedtest.log 2>&1 || \
-    ./speedtest-cli/speedtest --progress=no --server-id=$1 --accept-license --accept-gdpr > ./speedtest-cli/speedtest.log 2>&1
-    if [ $? -eq 0 ]; then
-        local dl_speed=$(awk '/Download/{print $3" "$4}' ./speedtest-cli/speedtest.log)
-        local up_speed=$(awk '/Upload/{print $3" "$4}' ./speedtest-cli/speedtest.log)
-        local latency=$(awk '/Latency/{print $2" "$3}' ./speedtest-cli/speedtest.log)
-        if [[ -n "${dl_speed}" && -n "${up_speed}" && -n "${latency}" ]]; then
-            printf "\033[0;33m%-18s\033[0;32m%-18s\033[0;31m%-20s\033[0;36m%-12s\033[0m\n" " ${nodeName}" "${up_speed}" "${dl_speed}" "${latency}"
-        fi
-    fi
+# disk_test
+# Purpose: This method is designed to test the disk performance of the host using the partition that the
+#          script is being run from using fio random read/write speed tests.
+# Parameters:
+#          - (none)
+function disk_test {
 }
+
+# dd_test
+# Purpose: This method is invoked if the fio disk test failed. dd sequential speed tests are
+#          not indiciative or real-world results, however, some form of disk speed measure 
+#          is better than nothing.
+# Parameters:
+#          - (none)
 function dd_test {
-	I=0
-	DISK_WRITE_TEST_RES=()
-	DISK_READ_TEST_RES=()
-	DISK_WRITE_TEST_AVG=0
-	DISK_READ_TEST_AVG=0
-
-	# run the disk speed tests (write and read) thrice over
-	while [ $I -lt 3 ]
-	do
-		# write test using dd, "direct" flag is used to test direct I/O for data being stored to disk
-		DISK_WRITE_TEST=$(dd if=/dev/zero of=$DISK_PATH/$DATE.test bs=64k count=16k oflag=direct |& grep copied | awk '{ print $(NF-1) " " $(NF)}')
-		VAL=$(echo $DISK_WRITE_TEST | cut -d " " -f 1)
-		[[ "$DISK_WRITE_TEST" == *"GB"* ]] && VAL=$(awk -v a="$VAL" 'BEGIN { print a * 1000 }')
-		DISK_WRITE_TEST_RES+=( "$DISK_WRITE_TEST" )
-		DISK_WRITE_TEST_AVG=$(awk -v a="$DISK_WRITE_TEST_AVG" -v b="$VAL" 'BEGIN { print a + b }')
-
-		# read test using dd using the 1G file written during the write test
-		DISK_READ_TEST=$(dd if=$DISK_PATH/$DATE.test of=/dev/null bs=8k |& grep copied | awk '{ print $(NF-1) " " $(NF)}')
-		VAL=$(echo $DISK_READ_TEST | cut -d " " -f 1)
-		[[ "$DISK_READ_TEST" == *"GB"* ]] && VAL=$(awk -v a="$VAL" 'BEGIN { print a * 1000 }')
-		DISK_READ_TEST_RES+=( "$DISK_READ_TEST" )
-		DISK_READ_TEST_AVG=$(awk -v a="$DISK_READ_TEST_AVG" -v b="$VAL" 'BEGIN { print a + b }')
-
-		I=$(( $I + 1 ))
-	done
-	# calculate the write and read speed averages using the results from the three runs
-	DISK_WRITE_TEST_AVG=$(awk -v a="$DISK_WRITE_TEST_AVG" 'BEGIN { print a / 3 }')
-	DISK_READ_TEST_AVG=$(awk -v a="$DISK_READ_TEST_AVG" 'BEGIN { print a / 3 }')
 }
 
 # check if disk performance is being tested and the host has required space (2G)
@@ -253,15 +225,20 @@ elif [ -z "$SKIP_FIO" ]; then
 	fi
 fi
 
-# iperf_test
-# Purpose: This method is designed to test the network performance of the host by executing an
-#          iperf3 test to/from the public iperf server passed to the function. Both directions 
-#          (send and receive) are tested.
-# Parameters:
-#          1. URL - URL/domain name of the iperf server
-#          2. PORTS - the range of ports on which the iperf server operates
-#          3. HOST - the friendly name of the iperf server host/owner
-#          4. FLAGS - any flags that should be passed to the iperf command
+speed_test() {
+    local nodeName="$2"
+    [ -z "$1" ] && ./speedtest-cli/speedtest --progress=no --accept-license --accept-gdpr > ./speedtest-cli/speedtest.log 2>&1 || \
+    ./speedtest-cli/speedtest --progress=no --server-id=$1 --accept-license --accept-gdpr > ./speedtest-cli/speedtest.log 2>&1
+    if [ $? -eq 0 ]; then
+        local dl_speed=$(awk '/Download/{print $3" "$4}' ./speedtest-cli/speedtest.log)
+        local up_speed=$(awk '/Upload/{print $3" "$4}' ./speedtest-cli/speedtest.log)
+        local latency=$(awk '/Latency/{print $2" "$3}' ./speedtest-cli/speedtest.log)
+        if [[ -n "${dl_speed}" && -n "${up_speed}" && -n "${latency}" ]]; then
+            printf "\033[0;33m%-18s\033[0;32m%-18s\033[0;31m%-20s\033[0;36m%-12s\033[0m\n" " ${nodeName}" "${up_speed}" "${dl_speed}" "${latency}"
+        fi
+    fi
+}
+
 speed() {
     speed_test '' 'Speedtest.net'
     speed_test '21541' 'Los Angeles, US'
