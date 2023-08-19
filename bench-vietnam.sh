d440bbc7538ee170e22b3783ae82eff628d93fd6
wpbai <139647725+wpbai@users.noreply.github.com>
2023-08-20 00:34:17 +0700

    Update bench-vietnam.sh

--- bench-vietnam.sh
+++ bench-vietnam.sh
@@ -68,7 +68,200 @@ speed_test() {
         fi
     fi
 }
+function dd_test {
+	I=0
+	DISK_WRITE_TEST_RES=()
+	DISK_READ_TEST_RES=()
+	DISK_WRITE_TEST_AVG=0
+	DISK_READ_TEST_AVG=0
 
+	# run the disk speed tests (write and read) thrice over
+	while [ $I -lt 3 ]
+	do
+		# write test using dd, "direct" flag is used to test direct I/O for data being stored to disk
+		DISK_WRITE_TEST=$(dd if=/dev/zero of=$DISK_PATH/$DATE.test bs=64k count=16k oflag=direct |& grep copied | awk '{ print $(NF-1) " " $(NF)}')
+		VAL=$(echo $DISK_WRITE_TEST | cut -d " " -f 1)
+		[[ "$DISK_WRITE_TEST" == *"GB"* ]] && VAL=$(awk -v a="$VAL" 'BEGIN { print a * 1000 }')
+		DISK_WRITE_TEST_RES+=( "$DISK_WRITE_TEST" )
+		DISK_WRITE_TEST_AVG=$(awk -v a="$DISK_WRITE_TEST_AVG" -v b="$VAL" 'BEGIN { print a + b }')
+
+		# read test using dd using the 1G file written during the write test
+		DISK_READ_TEST=$(dd if=$DISK_PATH/$DATE.test of=/dev/null bs=8k |& grep copied | awk '{ print $(NF-1) " " $(NF)}')
+		VAL=$(echo $DISK_READ_TEST | cut -d " " -f 1)
+		[[ "$DISK_READ_TEST" == *"GB"* ]] && VAL=$(awk -v a="$VAL" 'BEGIN { print a * 1000 }')
+		DISK_READ_TEST_RES+=( "$DISK_READ_TEST" )
+		DISK_READ_TEST_AVG=$(awk -v a="$DISK_READ_TEST_AVG" -v b="$VAL" 'BEGIN { print a + b }')
+
+		I=$(( $I + 1 ))
+	done
+	# calculate the write and read speed averages using the results from the three runs
+	DISK_WRITE_TEST_AVG=$(awk -v a="$DISK_WRITE_TEST_AVG" 'BEGIN { print a / 3 }')
+	DISK_READ_TEST_AVG=$(awk -v a="$DISK_READ_TEST_AVG" 'BEGIN { print a / 3 }')
+}
+
+# check if disk performance is being tested and the host has required space (2G)
+AVAIL_SPACE=$(df -k . | awk 'NR==2{print $4}')
+if [[ -z "$SKIP_FIO" && "$AVAIL_SPACE" -lt 2097152 && "$ARCH" != "aarch64" && "$ARCH" != "arm" ]]; then # 2GB = 2097152KB
+	echo -e "\nLess than 2GB of space available. Skipping disk test..."
+elif [[ -z "$SKIP_FIO" && "$AVAIL_SPACE" -lt 524288 && ("$ARCH" = "aarch64" || "$ARCH" = "arm") ]]; then # 512MB = 524288KB
+	echo -e "\nLess than 512MB of space available. Skipping disk test..."
+# if the skip disk flag was set, skip the disk performance test, otherwise test disk performance
+elif [ -z "$SKIP_FIO" ]; then
+	# Perform ZFS filesystem detection and determine if we have enough free space according to spa_asize_inflation
+	ZFSCHECK="/sys/module/zfs/parameters/spa_asize_inflation"
+	if [[ -f "$ZFSCHECK" ]];then
+		mul_spa=$((($(cat /sys/module/zfs/parameters/spa_asize_inflation)*2)))
+		warning=0
+		poss=()
+
+		for pathls in $(df -Th | awk '{print $7}' | tail -n +2)
+		do
+			if [[ "${PWD##$pathls}" != "${PWD}" ]]; then
+				poss+=($pathls)
+			fi
+		done
+
+		long=""
+		m=-1
+		for x in ${poss[@]}
+		do
+			if [ ${#x} -gt $m ];then
+				m=${#x}
+				long=$x
+			fi
+		done
+
+		size_b=$(df -Th | grep -w $long | grep -i zfs | awk '{print $5}' | tail -c 2 | head -c 1)
+		free_space=$(df -Th | grep -w $long | grep -i zfs | awk '{print $5}' | head -c -2)
+
+		if [[ $size_b == 'T' ]]; then
+			free_space=$(bc <<< "$free_space*1024")
+			size_b='G'
+		fi
+
+		if [[ $(df -Th | grep -w $long) == *"zfs"* ]];then
+
+			if [[ $size_b == 'G' ]]; then
+				if [[ $(echo "$free_space < $mul_spa" | bc) -ne 0 ]];then
+					warning=1
+				fi
+			else
+				warning=1
+			fi
+
+		fi
+
+		if [[ $warning -eq 1 ]];then
+			echo -en "\nWarning! You are running YABS on a ZFS Filesystem and your disk space is too low for the fio test. Your test results will be inaccurate. You need at least $mul_spa GB free in order to complete this test accurately. For more information, please see https://github.com/masonr/yet-another-bench-script/issues/13\n"
+		fi
+	fi
+	
+	echo -en "\nPreparing system for disk tests..."
+
+	# create temp directory to store disk write/read test files
+	DISK_PATH=$YABS_PATH/disk
+	mkdir -p $DISK_PATH
+
+	if [[ -z "$PREFER_BIN" && ! -z "$LOCAL_FIO" ]]; then # local fio has been detected, use instead of pre-compiled binary
+		FIO_CMD=fio
+	else
+		# download fio binary
+		if [[ ! -z $LOCAL_CURL ]]; then
+			curl -s --connect-timeout 5 --retry 5 --retry-delay 0 https://raw.githubusercontent.com/masonr/yet-another-bench-script/master/bin/fio/fio_$ARCH -o $DISK_PATH/fio
+		else
+			wget -q -T 5 -t 5 -w 0 https://raw.githubusercontent.com/masonr/yet-another-bench-script/master/bin/fio/fio_$ARCH -O $DISK_PATH/fio
+		fi
+
+		if [ ! -f "$DISK_PATH/fio" ]; then # ensure fio binary download successfully
+			echo -en "\r\033[0K"
+			echo -e "Fio binary download failed. Running dd test as fallback...."
+			DD_FALLBACK=True
+		else
+			chmod +x $DISK_PATH/fio
+			FIO_CMD=$DISK_PATH/fio
+		fi
+	fi
+
+	if [ -z "$DD_FALLBACK" ]; then # if not falling back on dd tests, run fio test
+		echo -en "\r\033[0K"
+
+		# init global array to store disk performance values
+		declare -a DISK_RESULTS DISK_RESULTS_RAW
+		# disk block sizes to evaluate
+		BLOCK_SIZES=( "4k" "64k" "512k" "1m" )
+
+		# execute disk performance test
+		disk_test "${BLOCK_SIZES[@]}"
+	fi
+
+	if [[ ! -z "$DD_FALLBACK" || ${#DISK_RESULTS[@]} -eq 0 ]]; then # fio download failed or test was killed or returned an error, run dd test instead
+		if [ -z "$DD_FALLBACK" ]; then # print error notice if ended up here due to fio error
+			echo -e "fio disk speed tests failed. Run manually to determine cause.\nRunning dd test as fallback..."
+		fi
+
+		dd_test
+
+		# format the speed averages by converting to GB/s if > 1000 MB/s
+		if [ $(echo $DISK_WRITE_TEST_AVG | cut -d "." -f 1) -ge 1000 ]; then
+			DISK_WRITE_TEST_AVG=$(awk -v a="$DISK_WRITE_TEST_AVG" 'BEGIN { print a / 1000 }')
+			DISK_WRITE_TEST_UNIT="GB/s"
+		else
+			DISK_WRITE_TEST_UNIT="MB/s"
+		fi
+		if [ $(echo $DISK_READ_TEST_AVG | cut -d "." -f 1) -ge 1000 ]; then
+			DISK_READ_TEST_AVG=$(awk -v a="$DISK_READ_TEST_AVG" 'BEGIN { print a / 1000 }')
+			DISK_READ_TEST_UNIT="GB/s"
+		else
+			DISK_READ_TEST_UNIT="MB/s"
+		fi
+
+		# print dd sequential disk speed test results
+		echo -e
+		echo -e "dd Sequential Disk Speed Tests:"
+		echo -e "---------------------------------"
+		printf "%-6s | %-6s %-4s | %-6s %-4s | %-6s %-4s | %-6s %-4s\n" "" "Test 1" "" "Test 2" ""  "Test 3" "" "Avg" ""
+		printf "%-6s | %-6s %-4s | %-6s %-4s | %-6s %-4s | %-6s %-4s\n"
+		printf "%-6s | %-11s | %-11s | %-11s | %-6.2f %-4s\n" "Write" "${DISK_WRITE_TEST_RES[0]}" "${DISK_WRITE_TEST_RES[1]}" "${DISK_WRITE_TEST_RES[2]}" "${DISK_WRITE_TEST_AVG}" "${DISK_WRITE_TEST_UNIT}" 
+		printf "%-6s | %-11s | %-11s | %-11s | %-6.2f %-4s\n" "Read" "${DISK_READ_TEST_RES[0]}" "${DISK_READ_TEST_RES[1]}" "${DISK_READ_TEST_RES[2]}" "${DISK_READ_TEST_AVG}" "${DISK_READ_TEST_UNIT}" 
+	else # fio tests completed successfully, print results
+		[[ ! -z $JSON ]] && JSON_RESULT+=',"fio":['
+		DISK_RESULTS_NUM=$(expr ${#DISK_RESULTS[@]} / 6)
+		DISK_COUNT=0
+
+		# print disk speed test results
+		echo -e "fio Disk Speed Tests (Mixed R/W 50/50):"
+		echo -e "---------------------------------"
+
+		while [ $DISK_COUNT -lt $DISK_RESULTS_NUM ] ; do
+			if [ $DISK_COUNT -gt 0 ]; then printf "%-10s | %-20s | %-20s\n"; fi
+			printf "%-10s | %-11s %8s | %-11s %8s\n" "Block Size" "${BLOCK_SIZES[DISK_COUNT]}" "(IOPS)" "${BLOCK_SIZES[DISK_COUNT+1]}" "(IOPS)"
+			printf "%-10s | %-11s %8s | %-11s %8s\n" "  ------" "---" "---- " "----" "---- "
+			printf "%-10s | %-11s %8s | %-11s %8s\n" "Read" "${DISK_RESULTS[DISK_COUNT*6+1]}" "(${DISK_RESULTS[DISK_COUNT*6+4]})" "${DISK_RESULTS[(DISK_COUNT+1)*6+1]}" "(${DISK_RESULTS[(DISK_COUNT+1)*6+4]})"
+			printf "%-10s | %-11s %8s | %-11s %8s\n" "Write" "${DISK_RESULTS[DISK_COUNT*6+2]}" "(${DISK_RESULTS[DISK_COUNT*6+5]})" "${DISK_RESULTS[(DISK_COUNT+1)*6+2]}" "(${DISK_RESULTS[(DISK_COUNT+1)*6+5]})"
+			printf "%-10s | %-11s %8s | %-11s %8s\n" "Total" "${DISK_RESULTS[DISK_COUNT*6]}" "(${DISK_RESULTS[DISK_COUNT*6+3]})" "${DISK_RESULTS[(DISK_COUNT+1)*6]}" "(${DISK_RESULTS[(DISK_COUNT+1)*6+3]})"
+			if [ ! -z $JSON ]; then
+				JSON_RESULT+='{"bs":"'${BLOCK_SIZES[DISK_COUNT]}'","speed_r":'${DISK_RESULTS_RAW[DISK_COUNT*6+1]}',"iops_r":'${DISK_RESULTS_RAW[DISK_COUNT*6+4]}
+				JSON_RESULT+=',"speed_w":'${DISK_RESULTS_RAW[DISK_COUNT*6+2]}',"iops_w":'${DISK_RESULTS_RAW[DISK_COUNT*6+5]}',"speed_rw":'${DISK_RESULTS_RAW[DISK_COUNT*6]}
+				JSON_RESULT+=',"iops_rw":'${DISK_RESULTS_RAW[DISK_COUNT*6+3]}',"speed_units":"KBps"},'
+				JSON_RESULT+='{"bs":"'${BLOCK_SIZES[DISK_COUNT+1]}'","speed_r":'${DISK_RESULTS_RAW[(DISK_COUNT+1)*6+1]}',"iops_r":'${DISK_RESULTS_RAW[(DISK_COUNT+1)*6+4]}
+				JSON_RESULT+=',"speed_w":'${DISK_RESULTS_RAW[(DISK_COUNT+1)*6+2]}',"iops_w":'${DISK_RESULTS_RAW[(DISK_COUNT+1)*6+5]}',"speed_rw":'${DISK_RESULTS_RAW[(DISK_COUNT+1)*6]}
+				JSON_RESULT+=',"iops_rw":'${DISK_RESULTS_RAW[(DISK_COUNT+1)*6+3]}',"speed_units":"KBps"},'
+			fi
+			DISK_COUNT=$(expr $DISK_COUNT + 2)
+		done
+		[[ ! -z $JSON ]] && JSON_RESULT=${JSON_RESULT::${#JSON_RESULT}-1} && JSON_RESULT+=']'
+	fi
+fi
+
+# iperf_test
+# Purpose: This method is designed to test the network performance of the host by executing an
+#          iperf3 test to/from the public iperf server passed to the function. Both directions 
+#          (send and receive) are tested.
+# Parameters:
+#          1. URL - URL/domain name of the iperf server
+#          2. PORTS - the range of ports on which the iperf server operates
+#          3. HOST - the friendly name of the iperf server host/owner
+#          4. FLAGS - any flags that should be passed to the iperf command
 speed() {
     speed_test '' 'Speedtest.net'
     speed_test '21541' 'Los Angeles, US'
